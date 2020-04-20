2e6d31f345a5aeca89d61139320cf5b9b7e0ffeb
Thomas Carstensen <tcarstensen489@gmail.com>
2020-04-20 11:23:36 +0200

    Create page

--- Assignment2/Controllers/HomeController.cs
+++ Assignment2/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Assignment2.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Assignment2.Controllers
 {
@@ -22,22 +23,31 @@ namespace Assignment2.Controllers
         {
             return View();
         }
-
+        [Authorize(Roles = "Waiter")]
         public IActionResult RestaurantView()
         {
             var viewModel = new RestaurantViewModel();
             return View(viewModel);
         }
-
+        [Authorize(Roles = "Kitchen")]
         public IActionResult KitchenView()
         {
             var viewModel = new KitchenViewModel();
             return View(viewModel);
         }
+
+        [Authorize(Roles = "Reception")]
         public IActionResult ReceptionView()
         {
             var viewModel = new ReceptionViewModel();
             return View(viewModel);
+        }
+
+        [Authorize(Roles = "Reception")]
+        public IActionResult CreateBreakfastOrder()
+        {
+            var obj = new BreakfastOrder();
+            return View(obj);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
